ecfdf5b3a3d90fc67d8797906aa22a588112c9fd
Sorry mate <sorrymate@MacBook-Pro-Sorry.local>
2021-11-05 00:34:46 +0500

    all staff added

---
+++ newMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {41,22,32,43,120,64,7,8,9,120};
+
+int n = array.Length;;
+int find = 120;
+
+int index = 0;
+
+while(index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
